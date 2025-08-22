2c2d90bcde2e195d93b4e87db98677a9eaf0e904
Drei Abmab <dreiabmab@gmail.com>
2025-08-22 22:41:56 +0800

    Improved Footer Position

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+// Make better
+
 const Footer = () => {
   return (
-    <div className="relative h-20 p-5 flex justify-center align-middle z-50 bg-gradient-to-r from-red-500 to-pink-500 text-white">
+    <div className="relative p-5 flex justify-center align-middle z-50 bg-gradient-to-r from-red-500 to-pink-500 text-white">
         <p>Created By Drei Abmab</p>
     </div>
   )
