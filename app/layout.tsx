657521bfe71213eea2b6f2bfba05d244bb952de0
Drei Abmab <dreiabmab@gmail.com>
2025-08-22 22:39:33 +0800

    Added Basic Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
           <div className="h-full flex flex-col">
             <Navbar />
             {children}
+            <Footer />
           </div>
         </AuthProvider>
       </body>
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="relative h-20 p-5 flex justify-center align-middle z-50 bg-gradient-to-r from-red-500 to-pink-500 text-white">
+        <p>Created By Drei Abmab</p>
+    </div>
+  )
+}
+
+export default Footer
